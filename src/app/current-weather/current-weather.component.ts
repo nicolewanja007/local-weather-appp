e2bd9aae15278d349a41907c9562ca438fee8551
nicolewanja007 <59738176+nicolewanja007@users.noreply.github.com>
2020-02-08 08:05:34 -0800

    changes on current weather

---
+++ src/app/current-weather/current-weather.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ICurrentWeather } from '../icurrent-weather';
+import { userInfo } from 'os';
+
+@Component({
+  selector: 'app-current-weather',
+  templateUrl: './current-weather.component.html',
+  styleUrls: ['./current-weather.component.css']
+})
+export class CurrentWeatherComponent implements OnInit {
+current: ICurrentWeather
+contructer() {
+    current = {
+      city:'andover',
+      contry: 'us'
+      date: 1486789600,
+      image: '',
+      temperature:4,
+      descripation:40 
+      
+
+    }
+  }
+ngOnInit(): void {
+  }
+
+}
---
+++ src/app/icurrent-weather.ts
@@ -0,0 +1,7 @@
+export interface ICurrentWeather {
+  city:string
+  country: string
+  date: number
+    temperature: number
+    description: string
+    image: string
